d6ce4502751c126e1b8613ad0efb0ff04d94ad6b
Suong Khonmarady <maradybor@gmail.com>
2025-06-29 06:50:37 +0700

    update icon

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Tos der leng app',
+        short_name: 'tosderleng',
+        description: 'ចង់ទៅណា? Plan trips, track expenses, and collaborate with travel companions',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#42b883', // change to your brand color
+        icons: [
+          {
+            src: '/icons/image.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/image.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
-    chunkSizeWarningLimit: 1000, // in kB
+    chunkSizeWarningLimit: 1000
   },
-  base: '/',
-  plugins: [react()],
-})
+  base: '/'
+});
